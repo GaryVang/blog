f8a18bd0ff1f67d444d3f2f2a1cacf6e32c35a35
MysticV <leng_vang89@hotmail.com>
2020-10-20 13:00:32 -0700

    Mini panel's header is now placed correctly. Added preliminary tagging feature to indicate blog post category.

--- src/components/Post/PostPanelMini.js
+++ src/components/Post/PostPanelMini.js
@@ -16,18 +16,22 @@ const PostPanelMini = ({ post, enableFullView }) => {
       className="post-panel-mini-container"
       onClick={() => enableFullView(post)}
     >
-      <header>
+      <header className="post-panel-mini-header">
+        <span className='tag test-tag'>Uncategorized</span>
+        {/* <span className='tag test-tag'>{String.fromCodePoint('0x1F60A')}</span> */}
         <h1 className="post-panel-mini-title">{post.title}</h1>
         <span className="post-panel-mini-publish-wrapper">
           <span>
-            By <a className="post-panel-mini-username" onClick={(e) => {handleUsernameClick(e);}}>
+            By: <a className="post-panel-mini-username" onClick={(e) => {handleUsernameClick(e);}}>
               {post.username}
             </a>
+            <span> @ <time>{post.post_date}</time></span>
           </span>
-          <time>Published {post.post_date}</time>
+          {/* <span> On <time>{post.post_date}</time></span> */}
+          {/* <time>Published {post.post_date}</time> */}
         </span>
-        <p className="post-panel-mini-snippet">{post.body}</p>
       </header>
+      <p className="post-panel-mini-snippet">{post.body}</p>
     </article>
   );
 };
